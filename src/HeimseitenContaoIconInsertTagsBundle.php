65a9666d640b40367259f2924eb9a03dad0157e7
Niels <info@heimseiten.de>
2026-06-23 11:29:27 +0200

    Modernize bundle layout (AbstractBundle, resources at root)

--- src/HeimseitenContaoIconInsertTagsBundle.php
+++ src/HeimseitenContaoIconInsertTagsBundle.php
@@ -2,33 +2,16 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of contao-icon-insert-tags-bundle.
- *
- * (c) heimseiten.de - Webdesign aus Köln 2025 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-icon-insert-tags-bundle
- */
-
 namespace Heimseiten\ContaoIconInsertTagsBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class HeimseitenContaoIconInsertTagsBundle extends Bundle
+class HeimseitenContaoIconInsertTagsBundle extends AbstractBundle
 {
-    public function getPath(): string
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        return \dirname(__DIR__);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
+        $container->import('../config/services.yaml');
     }
 }
